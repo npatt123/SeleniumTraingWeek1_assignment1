8cd779ba1c63f2d7e12fa74b2c87e08495757dcf
npatt123 <107314519+npatt123@users.noreply.github.com>
2022-06-13 20:52:35 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package week1.Day1;
+
+public class Factorial {
+
+	
+		
+	public static void main(String[] args) {
+		
+		int f = 1;
+		
+		for (int i = 5; i > 0 ; i-- ) {
+		 
+        f= f*i;
+    	
+		}
+		System.out.println(f);
+	
+	}
+
+}
