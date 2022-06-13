2791e6c81ef8c4729337eca76c07989e1e00e714
npatt123 <107314519+npatt123@users.noreply.github.com>
2022-06-13 14:49:20 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,35 @@
+package week1.Day1;
+
+
+
+public class FibonacciSeries {
+	
+	
+public static void main(String[] args) {
+	
+
+	int a=0;
+	int b=1;
+	int c;
+	
+	System.out.println(a);	
+	System.out.println(b);
+	
+	for(int i = 1; i <7; i++)
+	{
+		c= a+b;
+		System.out.println(c);
+		a=b;
+		b=c;
+				//System.out.println(c); 
+		//sys out statement, which line no it has to be placed? line no:21 or line no:24
+	
+	}
+}
+	
+	
+	
+
+
+
+}
