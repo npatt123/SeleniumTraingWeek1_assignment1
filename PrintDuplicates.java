afe582db5ec5951b151aa24fe0142d477ee3cf6b
npatt123 <107314519+npatt123@users.noreply.github.com>
2022-06-17 15:37:41 +0530

    Add files via upload

---
+++ PrintDuplicates.java
@@ -0,0 +1,50 @@
+package week1.Day2;
+
+import java.util.Arrays;
+
+public class PrintDuplicates {
+	
+	public static void main(String[] args) {
+		
+		int[] arrDuplicate= {14,12,13,11,15,14,18,16,17,19,18,17,20};
+		Arrays.sort(arrDuplicate);
+		System.out.println(arrDuplicate);
+		//int count= 0;
+		int firstNuum=0;
+		int secondNum=0;
+		
+		
+		for (int i = 0; i < arrDuplicate.length; i++) {
+			 firstNuum = arrDuplicate[i];
+			
+			for (int j= i+1; j < arrDuplicate.length; j++) {
+				 secondNum = arrDuplicate[j];
+				 break;
+				
+			}
+			if(firstNuum == secondNum) {
+				System.out.println("duplicate no: " + firstNuum);
+				
+			}
+		
+	}
+	}	
+
+	//int[] arr = {14,12,13,11,15,14,18,16,17,19,18,17,20};
+	
+	// get the length of the array
+	// declare an int variable named count
+	
+	// iterate from 0 to the array length-1 (outer loop starts here)
+	
+		// assign 0 to count 
+		
+		// iterate from i to the length of the array by adding 1 to it (inner loop starts here)
+		
+				// compare both the loop variables & check they're equal
+			
+						// increase the count if both the arrays are equal
+				
+		// Out of the inner loop, check and print the first array variable if count is more than 0
+
+}
